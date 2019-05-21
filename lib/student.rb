873a591c730ebd5b331841a1175ff4eeebaea313
Tony Baker <this.tonybaker@gmail.com>
2019-05-21 20:04:31 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,22 @@
 class Student
   attr_accessor :name, :grade, :id
 
+  def initialize(name, grade, id = nil)
+    @name = name
+    @grade = grade
+    @id = id
+  end
 
+  def save
+    sql = <<-SQL 
+      INSERT INTO student (name, grade)
+      VALUES (?, ?)
+    SQL
+
+    DB[:conn].execute(sql, self.name, self.grade)
+
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM student")[0][0]
+  end
+  
 
 end
